cb12db7885708c026d28c6139ee52fa58244a10a
Carolinealt <73542400+Carolinealt@users.noreply.github.com>
2024-07-01 15:27:44 +0300

    createSlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
-import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { actionMinus, actionPlus } from "../store/valueSlice/valueSlice";
+
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
 
 function App() {
-  const balance = useSelector((state) => {
-    return state.value.balance;
-  });
-  const dispatch = useDispatch();
-
-  const handlePlus = () => {
-    dispatch(actionPlus(1));
-  };
-
-  const handleMinus = () => {
-    dispatch(actionMinus(1));
-  };
   return (
     <>
-      <p>{balance}</p>
-      <p>BALANCE</p>
-      <button onClick={handlePlus}>+</button>
-      <button onClick={handleMinus}>-</button>
+      <div className="container">
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <SearchBox />
+        <ContactList />
+      </div>
     </>
   );
 }
--- store/store.js
+++
@@ -1,9 +0,0 @@
-import { composeWithDevTools } from "@redux-devtools/extension";
-import { combineReducers, createStore } from "redux";
-import { valueReducer } from "./valueSlice/valueSlice";
-
-const rootReducer = combineReducers({
-  value: valueReducer,
-});
-
-export const store = createStore(rootReducer, composeWithDevTools());
